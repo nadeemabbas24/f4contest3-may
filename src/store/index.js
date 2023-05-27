1d12ef148c0de77f89f6852cd6eaaab98f9b7ab3
nadeem abbas <nadeemmeerza@gmail.com>
2023-05-27 23:32:27 +0530

    second commit

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,6 @@ const initialState = {
   // Define the reducer function to handle state updates
   const reducer = (state = initialState, action) => {
     switch (action.type) {
-      case 'UPDATE_USERNAME':
-        return { ...state, username: action.payload };
-      case 'UPDATE_EMAIL':
-        return { ...state, email: action.payload };
-      case 'UPDATE_PASSWORD':
-        return { ...state, password: action.payload };
       case 'LOGOUT':
         return initialState;
       case 'LOGIN':
